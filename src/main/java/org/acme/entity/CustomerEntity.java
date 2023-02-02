2f5a1aa29e9c1dd380252d2e23b6041acd8cedbf
Francisco Sousa <francisco.sousa1987@outlook.com>
2023-02-02 12:53:36 -0300

    Criando classes Java Repository e Service

--- src/main/java/org/acme/entity/CustomerEntity.java
+++ src/main/java/org/acme/entity/CustomerEntity.java
@@ -1,5 +1,9 @@
 package org.acme.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -7,6 +11,9 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "customer")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerEntity {
 
     @Id
---
+++ src/main/java/org/acme/service/CustomerService.java
@@ -0,0 +1,44 @@
+package org.acme.service;
+
+import org.acme.dto.CustomerDTO;
+import org.acme.entity.CustomerEntity;
+import org.acme.repository.CustomerRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+public class CustomerService {
+
+    @Inject
+    private CustomerRepository customerRepository;
+
+    public List<CustomerDTO> findAllCustomers() {
+
+        List<CustomerDTO> customers = new ArrayList<>();
+
+        customerRepository.findAll().stream().forEach(item -> {
+            customers.add(mapCustomerEntityToDTO(item));
+        });
+
+        return customers;
+    }
+
+    private CustomerDTO mapCustomerEntityToDTO(CustomerEntity customer) {
+
+        CustomerDTO customerDTO = new CustomerDTO();
+
+        customerDTO.setAddress(customer.getAddress());
+        customerDTO.setAge(customer.getAge());
+        customerDTO.setEmail(customer.getEmail());
+        customerDTO.setName(customer.getName());
+        customerDTO.setPhone(customer.getPhone());
+
+        return customerDTO;
+
+    }
+}
+
+
